1727fc31ef2732facd6a29a1143246b6751edb45
Elena2609 <lenaalexse@gmail.com>
2026-09-15 10:52:39 +0300

    Первый коммит

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// int score = = 1200;
+// if (score >= 500){
+//     Console.WriteLine("Новичок");
+// };
+
+// Console.WriteLine("Игра продолжается");
+
+// int movieNumber = int.Parse(Console)
+using System.Data;
+
+// int score = 3200;
+// int recordScore = 3000;
+// string result = (score > recordScore) ?
+// "Новый рекорд!" : "Обычный результат!";
+// // if (score > recordScore)
+// //     result = "Новый рекорд!";
+// // else
+// //     result = "Обычный результат";
+// Console.WriteLine(result);
